1095cd6c947f23f6a0ee46f66859b52d8d010ea8
Mohit7089 <88561648+Mohit7089@users.noreply.github.com>
2025-04-02 14:33:05 +0530

    Update DatabaseHelper.java

--- loginpage/src/DatabaseHelper.java
+++ loginpage/src/DatabaseHelper.java
@@ -2,17 +2,12 @@ import java.sql.*;
 
 
 public class DatabaseHelper {
-   
-        // public static void main(String[] args) {
-        //     boolean result = validateUser("admin", "1234");
-        //     System.out.println("Login success: " + result);
-        // }
     
     
     
     private static final String URL = "jdbc:mysql://localhost:3306/userdb";
-    private static final String USER = "root";  // Change this to your MySQL username
-    private static final String PASSWORD = "mohit@7089";  // Change this to your MySQL password
+    private static final String USER = "root"; 
+    private static final String PASSWORD = "******"; 
 
     // Load MySQL Driver
     static {
@@ -33,14 +28,14 @@ public class DatabaseHelper {
             pstmt.setString(2, password);
 
             ResultSet rs = pstmt.executeQuery();
-            return rs.next(); // Returns true if user exists
+            return rs.next(); 
         } catch (SQLException e) {
             e.printStackTrace();
             return false;
         }
     }
 
-    // Method to register a new user
+
     public static boolean registerUser(String username, String password) {
         String query = "INSERT INTO users (username, password) VALUES (?, ?)";
         try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
